930a2a49039b7e43c4c54bfe0e33ad94a659e2b6
Coen Elferink <coen_elferink@hotmail.com>
2022-04-04 22:08:29 +0200

    Can mark X, can reset grid

--- script.js
+++ script.js
@@ -1,21 +1,45 @@
 // Module function to define gameBoard
 const gameBoard = (() => {
     const board = []
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            const cell = {
-                row: i,
-                col: j,
-                fill: ''
-            }
-            board.push(cell)
+    const getContainer = () => document.querySelector('.gameboard')
+    const createGrid = () => {
+        const container = getContainer()
+        for (let i = 0; i < 9; i++) {
+            const div = document.createElement('div')
+            div.dataset.index = i
+            div.classList.add('cell')
+            container.appendChild(div)
+            div.addEventListener('click', function() {
+                let cell = this.dataset.index
+                setCell(cell)
+            })
         }
     }
-    return { board }
+    const resetGrid = () => {
+        const divs = getContainer().childNodes
+        for (let i = 0; i < divs.length; i++) {
+            divs[i].remove()
+            i--
+        }
+        createGrid()
+    }
+    const setCell = (cell) => {
+        board[cell] = 'X'
+        let div = document.querySelector(`.cell[data-index="${cell}"]`)
+        div.innerText = "X"
+    }
+    return { createGrid, resetGrid }
 })();
 
 // Factory function to define players
 const Player = (symbol) => {
-    const symbol = symbol
     return { symbol }
 }
+
+const game = (() => {
+    const x = Player('X')
+    const o = Player('O')
+})()
+
+gameBoard.createGrid()
+
