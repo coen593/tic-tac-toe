f5a6977daa03c1ec3ccf78694674c261a460db03
Coen Elferink <coen_elferink@hotmail.com>
2022-04-06 20:17:07 +0200

    Developed Game Over logic

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Module function to define gameBoard
 const gameBoard = (() => {
     const board = []
+    let cell
     const getContainer = () => document.querySelector('.gameboard')
     const createGrid = () => {
         const container = getContainer()
@@ -10,8 +11,9 @@ const gameBoard = (() => {
             div.classList.add('cell')
             container.appendChild(div)
             div.addEventListener('click', function() {
-                let cell = this.dataset.index
-                setCell(cell)
+                if (this.innerText || game.gameOver) {return}
+                cell = this.dataset.index
+                game.playMove(this.dataset.index)
             })
         }
     }
@@ -23,22 +25,63 @@ const gameBoard = (() => {
         }
         createGrid()
     }
-    const setCell = (cell) => {
-        board[cell] = 'X'
+    const setCell = (active) => {
+        board[cell] = active
         let div = document.querySelector(`.cell[data-index="${cell}"]`)
-        div.innerText = "X"
+        div.innerText = active
     }
-    return { createGrid, resetGrid }
+    return { createGrid, setCell, board }
 })();
 
-// Factory function to define players
-const Player = (symbol) => {
-    return { symbol }
-}
-
 const game = (() => {
+    const Player = (symbol) => {
+        return { symbol }
+    }
     const x = Player('X')
     const o = Player('O')
+    let active = x
+    let gameOver
+
+    const playMove = () => {
+        gameBoard.setCell(active.symbol)
+        if (checkWin()) {
+            isGameOver(active.symbol)
+        }
+        console.log(gameOver)
+        active === x ? active = o : active = x
+    }
+
+    const checkWin = () => {
+        let board = gameBoard.board
+        const winConditions = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ]
+        for (let c of winConditions) { 
+            if (board[c[0]] === board[c[1]] && 
+            board[c[0]] === board[c[2]] &&
+            board[c[0]]) {
+                return true
+            }
+        }
+        return false
+    }
+
+    const isGameOver = (winner) => {
+        const modal = document.querySelector('.winner')
+        const wonText = document.querySelector('.won')
+        wonText.innerText = `${winner} won!`
+        modal.classList.add('active')
+        gameOver = true
+    }
+
+    return { playMove, gameOver }
 })()
 
 gameBoard.createGrid()
